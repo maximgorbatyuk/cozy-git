4957bfd59a94dfb8e58ec31b692dca43660b5175
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-01-30 09:46:52 +0500

    Initial Commit

---
+++ CozyGit/CozyGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CozyGitApp.swift
+//  CozyGit
+//
+//  Created by Maxim Gorbatyuk on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CozyGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
